d64533ef8f87c892f8589dedbbfaeeb4e8df9c4e
Christopher Zavala <76709102+BubbleheadGX@users.noreply.github.com>
2026-01-25 21:27:06 -0500

    Add basic login form validation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
 function redirectToRegistration() {
     window.location.href = "./signup.html";
 }
+
+function validateLoginForm() {
+    let error = false;
+
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value;
+
+    const errorMessageDisplay = document.createElement("div");
+    errorMessageDisplay.className = "error-message";
+    let errorMessageContent = "";
+
+    // Remove existing error message if any
+    const existingErrorMessage = document.querySelector(".error-message");
+    if (existingErrorMessage) {
+        existingErrorMessage.remove();
+    }
+
+    // Check for empty fields
+    if (!username || !password) {
+        errorMessageContent = "All fields are required.";
+        error = true;
+    }
+
+    if(error) {
+        errorMessageDisplay.textContent = errorMessageContent;
+        const passwordDiv = document.querySelector(".password-entry");
+        passwordDiv.after(errorMessageDisplay);
+    }
+
+    // add actual authentication later
+
+}
